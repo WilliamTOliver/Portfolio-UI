c2e5e01e40011de6a87b03b9aebc94545a533a5e
WilliamTOliver <WilliamTOliver@outlook.com>
2018-07-15 00:54:35 -0500

    update chart display

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,14 +12,25 @@ export class DashboardComponent implements OnInit {
   charts: any;
   chartData: any;
   loaded = false;
+  icons: any[] = ['scatter_plot', 'expand_less', 'show_chart', 'show_chart'];
   constructor(private configService: ConfigService, private chartService: ChartService ) {
     this.getChartSummaries();
   }
   ngOnInit() {
   }
   private async getChartSummaries() {
+    const comapre = (a, b) => {
+        if (a.sortWeight < b.sortWeight) {
+          return -1;
+         }
+       if (a.sortWeight > b.sortWeight) {
+         return 1;
+       }
+      return 0;
+    };
     const response = await this.chartService.getCharts();
-    this.chartData = response.data;
+    this.chartData = response.data.sort(comapre);
+    console.log(this.chartData)
     this.loaded = true;
   }
 }
